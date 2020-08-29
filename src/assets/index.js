0d93d7bcd8ab2d6dc6212b802fcd6d53fdc91303
Vineet Sridhar <vineet.sridhar@glyde.app>
2020-08-29 03:09:28 -0400

    Tried to clean up the code, and make it better

---
+++ src/assets/index.js
@@ -0,0 +1,69 @@
+
+import activity from "./activity.png"
+import hat from "./hat.png"
+import bulb from "./bulb.png"
+import code from "./code.png"
+import contact from './contact.png'
+import njit from "./njit.png"
+import java from "./java.png"
+import cplus from './cplus.png'
+import python from './python.png'
+import android from './androids.png'
+import csharp from './Csharp.png'
+import dart from './dart.png'
+import react from './react.png'
+import kotlin from './kotlin.png'
+import firebase from './firebase.png'
+import javascript from './javascript.png'
+import head from './bino.jpg'
+import headshot from './headshot.png'
+import arrow from './arrow.png'
+import html from './html.png'
+import css from './css.png'
+import photoshop from './photoshop.png'
+import acm from './acm.png'
+import woodbridge from './woodbridge.png'
+import bash from './bash.png'
+import js from './javascript.jpg'
+
+import mongo from './mongo.png';
+import git from './git.png'
+import graphql from './graphql.png'
+import aws from './aws.png'
+import nodejs from './nodejs.png'
+
+let images = {
+    mongo, 
+    git,
+    graphql,
+    aws,
+    nodejs,
+    activity, 
+    hat,
+    bulb,
+    code,
+    contact,
+    njit,
+    java,
+    cplus,
+    python,
+    android,
+    csharp,
+    dart,
+    react,
+    kotlin,
+    firebase,
+    javascript,
+    head,
+    headshot,
+    arrow,
+    html,
+    css,
+    photoshop,
+    acm,
+    woodbridge,
+    bash,
+    js
+}
+
+export default images

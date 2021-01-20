595f5499a403e6bcaf6dcb25de3d0f6d7686f24d
Vineet Sridhar <vineets1600@gmail.com>
2021-01-20 01:20:54 -0500

    Logos take up whole screen

--- src/screens/sections/TitleScreen.js
+++ src/screens/sections/TitleScreen.js
@@ -5,84 +5,69 @@ import { ZoomIn } from 'happy-helium'
 
 export default function TitleScreen({ mobile }) {
     let itemStyle = {
-        cursor: 'pointer', width: mobile ? '10vw' : '6vw'
+        cursor: 'pointer', width: mobile ? '15vw' : '6vw'
     }
     let wordStyle = {
         top: 5,
         marginBottom: '10px',
-        fontSize:'10',
+        fontSize: 10,
         cursor: 'pointer',
-        opacity:1
+        opacity: 1
     }
+    const links = [
+        {
+            link: "projects",
+            title: "Projects",
+            duration: 1200,
+            logo: images.code
+        },
+        {
+            link: "education",
+            title: "Education",
+            duration: 2500,
+            logo: images.hat
+        },
+        {
+            link: "activities",
+            title: "Activities",
+            duration: 2000,
+            logo: images.bulb
+        },
+        {
+            link: "contact",
+            title: "Contact",
+            duration: 2500,
+            logo:images.contact
+        },
+    ]
     return (
         <div className="sectionViewport">
-            {mobile ? null : <div className="line" />}
-
             <img src={images.squat}
                 className="pic" style={mobile ? { width: '50vw', height: '50vw', borderRadius: '25vw', objectFit: 'contain' } : {}} />
 
-            <text className="titleText" style={mobile ? { fontSize: '8vw' } : {}}>Vineet Sridhar</text>
+            <text className="titleText" style={mobile ? { fontSize: '10vw' } : {}}>Vineet Sridhar</text>
 
-            <text className="subtitleText" style={mobile ? { fontSize: '6vw' } : {}}>Personal Resume</text>
+            <text className="subtitleText" style={mobile ? { fontSize: '8vw' } : {}}>Personal Resume</text>
 
-            <div className="iconHolder" style={mobile ? { display: 'flex', flexDirection:'column' } : {}}>
+            <div className="iconHolder" style={mobile ? { display: 'flex', flexDirection: 'column' } : {}}>
+                {
+                    links.map(item => (
+                        <Link to={item.link} spy={true}
+                            smooth={true}
+                            duration={item.duration}>
+                            <ZoomIn>
+                                <div className="indiHolder" >
+                                    <img style={itemStyle} src={item.logo} className="picProps" />
+                                    <text style={mobile ? wordStyle : { cursor: 'pointer' }} className="wordProps">{item.title}</text>
+                                </div>
+                            </ZoomIn>
+                        </Link>
+                    ))
+                }
 
-                <Link to="projects" spy={true}
-                    smooth={true}
-                    duration={1200}>
-                    <ZoomIn>
-                        <div className="indiHolder" >
-                            <img style={itemStyle} src={images.code} className="picProps" />
-                            <text style={mobile? wordStyle : { cursor: 'pointer' }} className="wordProps">Projects</text>
-                        </div>
-                    </ZoomIn>
-                </Link>
-                <Link to="skills" spy={true}
-                    smooth={true}
-                    duration={1900}>
-                    <ZoomIn>
-                        <div className="indiHolder">
-                            <img style={itemStyle} src={images.activity} className="picProps" />
-                            <text style={mobile? wordStyle : { cursor: 'pointer' }} className="wordProps">Skills</text>
-                        </div>
-                    </ZoomIn>
-                </Link>
-
-                <Link to="education" spy={true}
-                    smooth={true}
-                    duration={2500}>
-                    <ZoomIn>
-                        <div className="indiHolder">
-                            <img style={itemStyle} src={images.hat} className="picProps" />
-                            <text style={mobile? wordStyle : { cursor: 'pointer' }} className="wordProps">Education</text>
-                        </div>
-                    </ZoomIn>
-                </Link>
-
-                <Link to="activities" spy={true}
-                    smooth={true}
-                    duration={2000}>
-                    <ZoomIn>
-                        <div className="indiHolder">
-                            <img style={itemStyle} src={images.bulb} className="picProps" />
-                            <text style={mobile? wordStyle : { cursor: 'pointer' }} className="wordProps">Activities</text>
-                        </div>
-                    </ZoomIn>
-                </Link>
-
-                <Link to="contact" spy={true}
-                    smooth={true}
-                    duration={2500}>
-                    <ZoomIn>
-                        <div className="indiHolder">
-                            <img style={itemStyle} src={images.contact} className="picProps" />
-                            <text style={mobile? wordStyle : { cursor: 'pointer' }} className="wordProps">Contact</text>
-                        </div>
-                    </ZoomIn>
-                </Link>
-                {mobile?<img className="arrow" src={images.arrow} style={mobile?{paddingTop:20}:{}} alt="arrow" />:null}
+                {mobile ? <img className="arrow" src={images.arrow} style={mobile ? { paddingTop: 20 } : {}} alt="arrow" /> : null}
             </div>
-            {mobile?null:<img className="arrow" src={images.arrow} style={mobile?{bottom:-10}:{}} alt="arrow" />}
+            {mobile ? null : <img className="arrow" src={images.arrow} style={mobile ? { bottom: -10 } : {}} alt="arrow" />}
         </div>
     )
 }
